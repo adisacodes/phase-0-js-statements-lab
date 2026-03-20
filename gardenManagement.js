1219480ea78257bccfaa2d231d4bed4aa904b51c
adisacodes <marian.adisa@student.moringaschool.com>
2026-03-20 20:28:45 +0300

    Add if-else statements and while loop for garden devices

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,25 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if (temperature >80) {
+    console.log("watering on");
+} else {
+    console.log("watering off");
+}
+
+if (timeOfDay === "evening" || timeOfDay === "night") {
+    console.log("lights on");
+} else {
+    console.log("lights off");
+}
+
+while (soilMoisture < 40) {
+    soilMoisture += 5;
+    console.log(soilMoisture);
+}
+
+if (soilMoisture=== 40) {
+    console.log(soilMoisture);
+}
+
